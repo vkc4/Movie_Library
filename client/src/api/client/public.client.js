ec4dd97c6208d2e621f50ceffa68e1f6f1bf809f
Vaibhav Kumar Choudhary <vaibhavkumar81990@gmail.com>
2024-05-31 17:44:56 +0530

    fix commit

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://movie-library-n93d.onrender.com/api/v1";
+const baseURL = "https://movie-library-5rfq.onrender.com/api/v1";
 
 const publicClient = axios.create({
   baseURL,
